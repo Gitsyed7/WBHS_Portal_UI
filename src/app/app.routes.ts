1ab8fef87892e8fd72923a9be96032da67e9834a
Gitsyed7 <syed.asik007@gmail.com>
2026-07-13 16:03:38 +0530

    Spinner mystery solved by adding signal to .ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
+
+
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./pages/home/home').then(m => m.Home)
   },
+  
 
   { path: 'about-us', loadComponent: () => import('./pages/about-us/about-us').then(m => m.AboutUs) },
   { path: 'contact-us', loadComponent: () => import('./pages/contact-us/contact-us').then(m => m.ContactUs) },
--- src/app/pages/home/circulars/circulars.ts
+++ src/app/pages/home/circulars/circulars.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CircularsService } from '../../../services/circulars.service';
 import { News } from '../../../models/news.model';
 import { ChangeDetectorRef } from '@angular/core';
@@ -15,7 +15,7 @@ export class Circulars implements OnInit {
   // Properties
   circulars: News[] = [];
 
-  loading = true;
+  loading = signal(true);
   // Constructor
   constructor(private circularsService: CircularsService,
   private cdr: ChangeDetectorRef) { }
@@ -35,7 +35,10 @@ export class Circulars implements OnInit {
 
       this.circulars = data;
       
-      this.loading = false;
+      this.loading.set(false);
+
+      
+
       
 //this.cdr.detectChanges();
 
@@ -52,7 +55,7 @@ console.log("View updated");
       console.log("4. ERROR");
       console.error(err);
 
-      this.loading = false;
+      this.loading.set(false);
 
     },
 
